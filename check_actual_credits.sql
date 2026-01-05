da60782667bf07a5ab7425727183467624f9c72b
Allen <dnbmashup1@gmail.com>
2026-01-05 17:57:40 +0000

    chore: final removal of legacy UI and unused files

--- check_actual_credits.sql
+++
@@ -1,4 +0,0 @@
--- Check actual credits in database for your user
-SELECT user_id, email, credits, updated_at 
-FROM profiles 
-WHERE email = 'altcoinsocial@gmail.com';
--- check_credits.sql
+++
@@ -1,4 +0,0 @@
--- Check current credits for the user
-SELECT user_id, email, credits, updated_at 
-FROM profiles 
-WHERE email = 'altcoinsocial@gmail.com';
